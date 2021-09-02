203c67c836c5c721e47cd6b012994ff02b6b2410
cyrine119 <84313207+cyrine119@users.noreply.github.com>
2021-09-02 03:27:38 +0200

    Add files via upload

---
+++ data-language.sql
@@ -0,0 +1,24 @@
+-- Create
+CREATE TABLE Product (
+    Product_id VARCHAR2(20) CONSTRAINT pk_product PRIMARY KEY,
+    Product_Name VARCHAR2(20) CONSTRAINT nn_product NOT NULL, 
+    Price NUMBER CONSTRAINT chk_Price CHECK (price>0)
+);
+
+CREATE TABLE Customer (
+    Customer_id VARCHAR2(20) CONSTRAINT pk_customer PRIMARY KEY,
+    Customer_Name VARCHAR2(20) CONSTRAINT nn_customer NOT NULL,
+    Customer_Tel NUMBER
+);
+
+CREATE TABLE Orders (
+    CONSTRAINT fk_Customer FOREIGN KEY (Customer_id) REFERENCES Customer (Customer_id),
+    CONSTRAINT fk_Product FOREIGN KEY (Product_id) REFERENCES Product (Product_id),
+    Quantity NUMBER,
+    Total_amount NUMBER
+);
+
+-- SQL Commands 
+ALTER TABLE Product ADD Category VARCHAR2(20); 
+ALTER TABLE Orders ADD OrderDate DATE DEFAULT SYSDATE()
+
